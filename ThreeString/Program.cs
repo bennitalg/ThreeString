3fb1658177b781beeb3c881c3ba8911804b5340b
bennitalg <bennitalg@outlook.com>
2018-08-19 22:07:12 -0500

    CG2-3

--- ThreeString/Program.cs
+++ ThreeString/Program.cs
@@ -10,6 +10,18 @@ namespace ThreeString
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("What is the make of your car?");
+            Console.Write("Type the make of your car: ");
+            string myMakeCar;
+            myMakeCar = Console.ReadLine();
+
+            string myModelCar;
+            Console.Write("Type the model of your car: ");
+            myModelCar = Console.ReadLine();
+
+            Console.WriteLine("My new car is a 2018 " + myMakeCar + " " + myModelCar.);
+            Console.ReadLine();
+
         }
     }
 }
